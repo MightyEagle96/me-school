2f502c69818f78fe3d3b54624aa4275b1b804824
Eagle Ikechukwu <mightyeaglecorp@gmail.com>
2021-10-02 11:35:11 +0100

    made some changes

--- src/pages/Teachers/Exams/ExamChoicePage.jsx
+++ src/pages/Teachers/Exams/ExamChoicePage.jsx
@@ -4,17 +4,21 @@ import Navbar from '../../../components/Navbar/Navbar';
 import Footer from '../../../components/Footer/Footer';
 import { StaffSideMenu } from '../../../components/SideMenu/StaffSideMenu/StaffSideMenu';
 import { SetExamChoiceItem } from '../../../components/ExamCards/SetExamChoiceItem';
-
+import { IsLoading } from '../../../assets/aesthetics/IsLoading';
 export default function ExamChoicePage() {
   const [subjects, setSubjects] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   async function getSubjects() {
+    setLoading(true);
     const path = 'subjects';
     const res = await httpService.get(path);
     if (res) {
       setSubjects(res.data.subjects);
+      setLoading(false);
     } else {
       console.log('error');
+      setLoading(false);
     }
   }
 
@@ -36,6 +40,15 @@ export default function ExamChoicePage() {
               </div>
             </div>
             <hr />
+            {loading ? (
+              <div className="text-center">
+                <div class="spinner-border text-primary" role="status">
+                  <span class="sr-only">Loading...</span>
+                </div>
+              </div>
+            ) : (
+              ''
+            )}
             <div className="d-flex flex-wrap">
               {subjects.map((subject, index) => {
                 return <SetExamChoiceItem key={index} {...subject} />;
