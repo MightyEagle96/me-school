93086e9b4657dca464456b6b8cd55f872bcadaf0
Dubem Ikechukwu <mightyeaglecorp@gmail.com>
2021-11-24 01:05:52 +0100

    made some changes

--- src/data/services.js
+++ src/data/services.js
@@ -1,18 +1,18 @@
-import axios from "axios";
+import axios from 'axios';
 
-import Swal from "sweetalert2";
-export const backendUrl = "http://localhost:4000/";
+import Swal from 'sweetalert2';
+//export const backendUrl = "http://localhost:4000/";
 //export const backendUrl = "http://192.168.1.2:4000/";
-//export const backendUrl = "https://mightyeagle-be.herokuapp.com";
+export const backendUrl = 'https://mightyeagle-be.herokuapp.com';
 
-const AUTH_TOKEN = localStorage.getItem("token") || "";
+const AUTH_TOKEN = localStorage.getItem('token') || '';
 
 export const httpService = axios.create({
   baseURL: backendUrl,
   timeout: 10000,
-  withCredentials: "include",
+  withCredentials: 'include',
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
     Authorization: `Bearer ${AUTH_TOKEN}`,
   },
 });
@@ -24,35 +24,35 @@ httpService.interceptors.response.use(
     if (response) {
       if (
         response.status === 401 &&
-        response.data.message === "Incorrect Email or Password"
+        response.data.message === 'Incorrect Email or Password'
       ) {
-        Swal.fire({ icon: "error", text: response.data.message });
+        Swal.fire({ icon: 'error', text: response.data.message });
       } else if (
         response.status === 401 &&
-        (response.data.message === "invalid token" ||
-          response.data.message === "invalid algorithm" ||
-          response.data.message === "jwt must be provided" ||
-          response.data.message === "jwt expired")
+        (response.data.message === 'invalid token' ||
+          response.data.message === 'invalid algorithm' ||
+          response.data.message === 'jwt must be provided' ||
+          response.data.message === 'jwt expired')
       ) {
         Swal.fire({
-          icon: "warning",
-          text: "Unavailable to validate token.",
+          icon: 'warning',
+          text: 'Unavailable to validate token.',
         }).then(() => {
-          localStorage.removeItem("token");
-          localStorage.removeItem("loggedInUser");
-          window.location.assign("/login");
+          localStorage.removeItem('token');
+          localStorage.removeItem('loggedInUser');
+          window.location.assign('/login');
         });
       } else if (response.status === 403) {
         Swal.fire({
-          icon: "warning",
-          titleText: "Access Denied",
+          icon: 'warning',
+          titleText: 'Access Denied',
           text: response.data.message,
         });
       } else if (response.status === 409) {
         Swal.fire({
-          icon: "error",
+          icon: 'error',
           titleText: "Can't create",
-          text: "Duplicate data found.",
+          text: 'Duplicate data found.',
         });
       }
     }
@@ -60,29 +60,29 @@ httpService.interceptors.response.use(
 );
 
 export const loggedInUser =
-  JSON.parse(localStorage.getItem("loggedInUser")) || null;
+  JSON.parse(localStorage.getItem('loggedInUser')) || null;
 
 class DataService {
   loggedInUser() {
-    return JSON.parse(localStorage.getItem("loggedInUser")) || null;
+    return JSON.parse(localStorage.getItem('loggedInUser')) || null;
   }
   async logout() {
     Swal.fire({
-      icon: "info",
+      icon: 'info',
       html: `<div class="spinner-border text-primary" role="status">
     <span class="sr-only">Loading...</span>
   </div>`,
       showConfirmButton: false,
-      title: "Logging out",
+      title: 'Logging out',
     });
-    const path = "auth/logout";
+    const path = 'auth/logout';
     await httpService
       .post(path, {})
       .then(() => {
-        localStorage.removeItem("token");
-        localStorage.removeItem("loggedInUser");
+        localStorage.removeItem('token');
+        localStorage.removeItem('loggedInUser');
       })
-      .then(() => window.location.assign("/"));
+      .then(() => window.location.assign('/'));
   }
 }
 
