a59edbe88cc9006bc3681f9b2acf279ef2e8d9f2
Dubem Ikechukwu <mightyeaglecorp@gmail.com>
2022-01-23 15:10:12 +0100

    made changes

--- src/pages/ClassTeacher/MyClassStudents.jsx
+++ src/pages/ClassTeacher/MyClassStudents.jsx
@@ -29,7 +29,7 @@ export default function MyClassStudentsPage() {
           <img
             src={`${backendUrl}/images/${rowData.imageUrl}`}
             alt={rowData.fullName}
-            className="avatar"
+            className="avatarList img-fluid"
           />
         </div>
       ),
